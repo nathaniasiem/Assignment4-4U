2fbd22551d5f778b000c9c17ec77a658806556a1
candy <candy@192.168.0.20>
2017-10-17 17:48:38 -0400

    17/10/2017-War Game Completed

--- src/exampleObject/War.java
+++ src/exampleObject/War.java
@@ -61,91 +61,95 @@ public class War {
             Card p2Card = p2.playCard();
             System.out.println("Player 2 plays " + p2Card);
 
-            //did a war happen?
+            //who wins?
             int winner = winner(p1Card, p2Card);
-
+            //did war happen?
             if (winner == 0) {
-            }
-            //declare war
-            System.out.println("WAR");
+                //create a pile of cards
+                ArrayList<Card> pile = new ArrayList<>();
 
-            ArrayList<Card> pile = new ArrayList<>();
-
-
-
-            //keep playing until someone wins
-            while (winner == 0) {
-                if (p1.getNumCards() < 4) {
-                    //player 1 loses
-                    winner = 2;
-                    break;
-                }
-                while (p1.getNumCards() > 0) {
-                    p1.playCard();
-                }
-                break;
-            }else if(p2.getNumCards () < 4){
-                
-            
-            }
-                
+                // keep playing until someone wins
+                while (winner == 0) {
+                    // make sure players have enough cards in hand
+                    if (p1.getNumCards() < 4) {
+                        System.out.println("Player 1 ran out of cards for a war");
+                        // p1 loses, p2 wins!
+                        winner = 2;
+                        // get rid of cards
+                        while (p1.getNumCards() > 0) {
+                            p1.playCard();
+                        }
+                        break;
+                    } else if (p2.getNumCards() < 4) {
+                        System.out.println("Player 2 ran out of cards for a war");
+                        // p2 loses, p1 wins!
+                        winner = 1;
+                        // get rid of cards
+                        while (p2.getNumCards() > 0) {
+                            p2.playCard();
+                        }
+                        break;
+                    }
+                    //declare war
+                    System.out.println("WAR");
                     //add in already played cards
                     //to keep cards otherwise cards in hand will disappear
-                    pile.add (p1Card);
-
-    pile.add (p2Card);
-    //draw 3 cards from
-    //each player to add to pile
-    Card[] p1Cards = p1.get3Cards();
-    Card[] p2Cards = p2.get3Cards();
-    for (int i = 0;
-    i< 3; i
-
-    
-        ++) {
+                    pile.add(p1Card);
+                    pile.add(p2Card);
+                    //draw 3 cards from
+                    //each player to add to pile
+                    Card[] p1Cards = p1.get3Cards();
+                    Card[] p2Cards = p2.get3Cards();
+                    for (int i = 0;
+                            i < 3; i++) {
                         pile.add(p1Cards[i]);
-        pile.add(p2Cards[i]);
-    }
-    //draw new card to player
-    p1Card  = p1.playCard();
-    p2Card  = p2.playCard();
+                        pile.add(p2Cards[i]);
+                    }
+                    //draw new card to player
+                    p1Card = p1.playCard();
+                    p2Card = p2.playCard();
 
-    System.out.println (
-            
-
-    "Player 1 plays " + p1Card);
-                    System.out.println (
-            
-    "Player 2 plays " + p2Card);
-                    winner  = winner(p1Card, p2Card);
-}
-//war is over
-//add pile to the victor
-if (winner == 1) {
+                    System.out.println(
+                            "Player 1 plays " + p1Card);
+                    System.out.println(
+                            "Player 2 plays " + p2Card);
+                    winner = winner(p1Card, p2Card);
+                }
+                //war is over
+                //add pile to the victor
+                if (winner == 1) {
                     //for each card in the pile
                     for (Card aCard : pile) {
                         p1.takeCard(aCard);
                     }
                 } else {
+                    //for each card in the pile
                     for (Card aCard : pile) {
                         p2.takeCard(aCard);
                     }
                 }
 
-
-
-            } else if (winner == 1) {
+            }
+            //player1 wins
+            if (winner == 1) {
+                //takes both cards
                 p1.takeCard(p2Card);
                 p1.takeCard(p1Card);
+                //player2 wins
             } else {
-
+                //take both cards
                 p2.takeCard(p1Card);
                 p2.takeCard(p2Card);
             }
-//space to separate rounds
+            //space to separate rounds
+            System.out.println("P1:" + p1.getNumCards() + " P2: " + p2.getNumCards());
             System.out.println("");
         }
-if(p1.getNumCards()>0){
-so
+        //player 1 wins!
+        if (p1.getNumCards() > 0) {
+            System.out.println("Player 1 WINS!");
+        } else {
+            System.out.println("Player 2 WINS!");
+        }
     }
 }
